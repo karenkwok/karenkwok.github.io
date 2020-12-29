fb6bfc84037f7d5f22be3c71331a0a0571ec3aa0
Karen Kwok <kkwok98@hotmail.com>
2020-12-29 14:51:09 -0500

    title typing name animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'personal-website';
+  firstName = 'K';
+  lastName = 'k';
+  ngOnInit(): void {
+    const fullFirstName = 'Karen';
+    const fullLastName = ' Kwok';
+    let firstNameIndex = 1;
+    let lastNameIndex = 3;
+    interval(500)
+      .pipe(
+        take(4),
+        tap(() => {
+          this.firstName = this.firstName + fullFirstName[firstNameIndex];
+          this.lastName = fullLastName[lastNameIndex] + this.lastName;
+          firstNameIndex++;
+          lastNameIndex--;
+        })
+      )
+      .subscribe();
+  }
 }

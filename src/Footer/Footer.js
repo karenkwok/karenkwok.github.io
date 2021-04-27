f4a0a1c6ee744393f8394d001012f483597953fb
Karen Kwok <kkwok98@hotmail.com>
2021-04-26 22:23:18 -0400

    added linkedin and github links in footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,2 +1,37 @@
 import "./Footer.scss";
 
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/karen-kwok/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-linkedin-in icon"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/karenkwok"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-github-alt icon"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <button>
+          <a href="mailto:karenk.kwok@mail.utoronto.ca">email</a>
+        </button>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;

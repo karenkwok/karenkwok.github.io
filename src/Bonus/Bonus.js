75fb54d851afc3e7245c052d97af6c7383ba9583
Karen Kwok <kkwok98@hotmail.com>
2021-04-27 04:04:29 -0400

    added bonus section

---
+++ src/Bonus/Bonus.js
@@ -0,0 +1,23 @@
+import "./Bonus.scss";
+
+function Bonus() {
+  return (
+    <div id="bonus-section">
+      <h2>A little bit more about me...</h2>
+      <h4 className="no-margin-bottom">
+        I'm also an amateur pianist 🎹, book lover 📚, and retired painter 🎨.
+      </h4>
+      <h4 className="no-margin-bottom yes-margin-top">
+        I have a pet turtle. We've been together for 10 years now! 🐢
+      </h4>
+      <h4 className="no-margin-bottom yes-margin-top">
+        My favourite foods are sushi 🍣, ice cream 🍦, and fried chicken 🍗.
+      </h4>
+      <h4 className="yes-margin-top">
+        Did I mention I'm a huuuge Pokémon fan?? 🤪
+      </h4>
+    </div>
+  );
+}
+
+export default Bonus;

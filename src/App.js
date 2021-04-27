4e47778cbee00167c9224cc2347f0466f88c738d
Karen Kwok <kkwok98@hotmail.com>
2021-04-27 02:43:15 -0400

    connected section components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.scss";
 import Header from "./Header/Header";
+import Title from "./Title/Title";
+import About from "./About/About";
+import Skills from "./Skills/Skills";
+import Projects from "./Projects/Projects";
+import Bonus from "./Bonus/Bonus";
 import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <div id="App">
       <Header></Header>
-      body
+      <Title></Title>
+      <About></About>
+      <Skills></Skills>
+      <Projects></Projects>
+      <Bonus></Bonus>
+      <div>
+        <h4>Thanks for stopping by! ❤️</h4>
+      </div>
       <Footer></Footer>
     </div>
   );

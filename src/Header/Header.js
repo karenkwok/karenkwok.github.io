24510450f0a3925ff45ec098e756a750310ca1da
Karen Kwok <kkwok98@hotmail.com>
2021-04-26 21:02:44 -0400

    added placeholder items in header

---
+++ src/Header/Header.js
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <div id="logo">
+          Kk
+      </div>
+      <div id="night-mode-toggle">
+          toggle
+      </div>
+    </header>
+  );
+}
+
+export default Header;

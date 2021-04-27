4fe66a68beea0abaaa84b9b782b3efe7b46bdfe6
Karen Kwok <kkwok98@hotmail.com>
2021-04-27 03:11:02 -0400

    added about section

---
+++ src/About/About.js
@@ -0,0 +1,16 @@
+import "./About.scss";
+
+function About() {
+  return (
+    <div id="about-section">
+      <h4>Hey, I'm Karen! 👋</h4>
+      <h4 id="about-second-line">
+        I'm a full stack developer and recent grad from the University of
+        Toronto.
+      </h4>
+      <h4 id="about-third-line">Currently, I'm open to opportunities!</h4>
+    </div>
+  );
+}
+
+export default About;

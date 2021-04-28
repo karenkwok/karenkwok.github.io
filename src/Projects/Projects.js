8a6127889bb4ba9c6fd27e3bd5865df43f9548ab
Karen Kwok <kkwok98@hotmail.com>
2021-04-27 21:51:54 -0400

    added purple highlight to links

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -4,7 +4,7 @@ function Projects() {
   return (
     <div id="projects-section">
       <h2>Here is what I've been working on...</h2>
-      <h4>For more details, please see my resume.</h4>
+      <h4>For more details, please see <a className="purple-highlight">my resume</a>.</h4>
     </div>
   );
 }

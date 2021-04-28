ae130c8086e7258b1ef33b050de5fa42c30ed3d6
Karen Kwok <kkwok98@hotmail.com>
2021-04-27 23:17:11 -0400

    formatted code

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -4,7 +4,10 @@ function Projects() {
   return (
     <div id="projects-section">
       <h2>Here is what I've been working on...</h2>
-      <h4>For more details, please see <a className="purple-highlight">my resume</a>.</h4>
+      <h4>
+        For more details, please see{" "}
+        <a className="purple-highlight">my resume</a>.
+      </h4>
     </div>
   );
 }

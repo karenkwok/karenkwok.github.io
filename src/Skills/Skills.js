11ab191076e3b98c4d184a2c8a3ac5fea47ec088
Karen Kwok <kkwok98@hotmail.com>
2021-04-27 23:55:48 -0400

    styled skills buttons

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -8,33 +8,33 @@ function Skills() {
         <div>
           <h3>Web</h3>
           <div className="skill-names">
-            <h5>React</h5>
-            <h5>Node.js</h5>
-            <h5>Express.js</h5>
-            <h5>SCSS</h5>
-            <h5>MongoDB</h5>
-            <h5>JavaScript</h5>
-            <h5>GraphQL</h5>
-            <h5>HTML</h5>
-            <h5>CSS</h5>
+            <h5 className="skill-bubble">React</h5>
+            <h5 className="skill-bubble">Node.js</h5>
+            <h5 className="skill-bubble">Express.js</h5>
+            <h5 className="skill-bubble">SCSS</h5>
+            <h5 className="skill-bubble">MongoDB</h5>
+            <h5 className="skill-bubble">JavaScript</h5>
+            <h5 className="skill-bubble">GraphQL</h5>
+            <h5 className="skill-bubble">HTML</h5>
+            <h5 className="skill-bubble">CSS</h5>
           </div>
         </div>
         <div>
           <h3>Languages</h3>
           <div className="skill-names">
-            <h5>Python</h5>
-            <h5>Java</h5>
-            <h5>C#</h5>
-            <h5>C</h5>
-            <h5>R</h5>
+            <h5 className="skill-bubble">Python</h5>
+            <h5 className="skill-bubble">Java</h5>
+            <h5 className="skill-bubble">C#</h5>
+            <h5 className="skill-bubble">C</h5>
+            <h5 className="skill-bubble">R</h5>
           </div>
         </div>
         <div>
           <h3>Technologies</h3>
           <div className="skill-names">
-            <h5>Heroku</h5>
-            <h5>Unity</h5>
-            <h5>Git</h5>
+            <h5 className="skill-bubble">Heroku</h5>
+            <h5 className="skill-bubble">Unity</h5>
+            <h5 className="skill-bubble">Git</h5>
           </div>
         </div>
       </div>

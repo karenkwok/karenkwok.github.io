90c6ff0ee15361022b40440be8423ded5dcddede
Karen Kwok <kkwok98@hotmail.com>
2021-05-03 17:38:55 -0400

    edited content and styles for neurorehab page

--- src/Projects/Project/Neurorehab/Neurorehab.js
+++ src/Projects/Project/Neurorehab/Neurorehab.js
@@ -7,26 +7,39 @@ function Neurorehab() {
 
   return (
     <div>
-      <div className="project-page-title">
-        <h2>NeuroRehab Project</h2>
-        <button onClick={history.goBack}>X</button>
+      <div className="project-page-titles">
+        <h2 className="project-page-title">NeuroRehab Project</h2>
+        <button onClick={history.goBack}>Back</button>
       </div>
-      <h4>July 2020 - August 2020 (1 month)</h4>
-      <div className="project-page-skills">
-        <h5>C#</h5>
-        <h5>Unity</h5>
+      <p>July 2020 - August 2020</p>
+      <div className="skill-names">
+        <h5 className="skill-bubble">C#</h5>
+        <h5 className="skill-bubble">Unity</h5>
       </div>
       <h4>
         As a volunteer for{" "}
-        <a href="https://sensetech.ca" target="_blank" rel="noreferrer">
+        <a
+          href="https://sensetech.ca"
+          target="_blank"
+          rel="noreferrer"
+          className="purple-highlight"
+        >
           SenseTech Solutions
         </a>
-        , I developed a VR piano simulator game for patients recovering from a
-        stroke to help them regain their motor and coordination skills.
+        , I developed a VR piano simulator game for patients recovering from
+        strokes to help them regain their motor and coordination skills.
       </h4>
       <h4>
-        The game teaches players how to play various songs with the Haptic
-        Glove, and displays performance statistics.
+        The game teaches players how to play various songs with the{" "}
+        <a
+          href="https://www.google.com/search?q=what+are+haptic+gloves&rlz=1C1CHBF_enCA772CA772&sxsrf=ALeKk03fNYeIO-WmdQxwTU6ZtBRZOn2pZg%3A1620018323928&ei=k4SPYJCPONS5tAbTsIjADw&oq=what+are+haptic+glove&gs_lcp=Cgdnd3Mtd2l6EAMYADICCAA6BwgjELADECc6BwgAEEcQsAM6BwgAELADEEM6BwgjELACECc6BAgAEA06BggAEAcQHlCKgQFY5YgBYMGUAWgBcAJ4AIABcogB3QeSAQM2LjSYAQCgAQGqAQdnd3Mtd2l6yAEKwAEB&sclient=gws-wiz"
+          target="_blank"
+          rel="noreferrer"
+          className="purple-highlight"
+        >
+          Haptic Glove
+        </a>
+        , and displays performance statistics.
       </h4>
     </div>
   );

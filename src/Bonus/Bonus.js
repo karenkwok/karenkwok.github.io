bca5bcb7e4fa3ab6c46c926852f4d90afe3abd63
Karen Kwok <kkwok98@hotmail.com>
2021-05-02 23:13:26 -0400

    formatted code

--- src/Bonus/Bonus.js
+++ src/Bonus/Bonus.js
@@ -6,27 +6,39 @@ function Bonus() {
     <div id="bonus-section">
       <h2>A little bit more about me...</h2>
       <h4 className="no-margin-bottom">
-        I'm also an <span class="bonus-bold">amateur pianist</span> 🎹, <span class="bonus-bold">book lover</span> 📚, and{" "}
-        <Link to="/art"><span class="bonus-bold purple-highlight">retired painter</span> 🎨</Link>.
+        I'm also an <span class="bonus-bold">amateur pianist</span> 🎹,{" "}
+        <span class="bonus-bold">book lover</span> 📚, and{" "}
+        <Link to="/art">
+          <span class="bonus-bold purple-highlight">retired painter</span> 🎨
+        </Link>
+        .
       </h4>
       <h4 className="no-margin-bottom yes-margin-top">
-        I have a <span class="bonus-bold">pet turtle</span>. We've been together for <span class="bonus-bold">10 years</span> now!
-        🐢
+        I have a <span class="bonus-bold">pet turtle</span>. We've been together
+        for <span class="bonus-bold">10 years</span> now! 🐢
       </h4>
       <h4 className="no-margin-bottom yes-margin-top">
-        My favourite foods are <span class="bonus-bold">sushi</span> 🍣, <Link to="/icecream"><span class="bonus-bold purple-highlight">ice cream</span> 🍦</Link>, and{" "}
+        My favourite foods are <span class="bonus-bold">sushi</span> 🍣,{" "}
+        <Link to="/icecream">
+          <span class="bonus-bold purple-highlight">ice cream</span> 🍦
+        </Link>
+        , and{" "}
         <a
           href="https://www.popeyeschicken.ca"
           target="_blank"
           rel="noreferrer"
           id="popeyes"
         >
-          <span id="chicken" class="bonus-bold">fried chicken</span> 🍗
+          <span id="chicken" class="bonus-bold">
+            fried chicken
+          </span>{" "}
+          🍗
         </a>{" "}
         .
       </h4>
       <h4 className="yes-margin-top">
-        Did I mention I'm a huuuge <span class="bonus-bold">Pokémon fan</span>?? 🤪
+        Did I mention I'm a huuuge <span class="bonus-bold">Pokémon fan</span>??
+        🤪
       </h4>
       <h4 id="bonus-last-line">Thanks for stopping by! ❤️</h4>
     </div>

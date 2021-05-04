deed6248bfb9231ccd7aa331e36f47b18702769b
Karen Kwok <kkwok98@hotmail.com>
2021-05-04 13:02:31 -0400

    removed hire me btn styles

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -28,7 +28,7 @@ function Footer() {
       </div>
       <div>
         <a href="mailto:karenk.kwok@mail.utoronto.ca?subject=You're Hired!&body=Congratulations! You got the job!!">
-          <Button variant="outlined" size="large" id="hire-me-btn">
+          <Button variant="outlined" size="large">
             <i class="far fa-paper-plane"></i>&nbsp;&nbsp;hire me
           </Button>
         </a>

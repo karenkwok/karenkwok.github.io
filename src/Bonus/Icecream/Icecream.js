e880e602e450dbcd2be6be9da512b043b8ef3ed8
Karen Kwok <kkwok98@hotmail.com>
2021-05-04 02:50:27 -0400

    added fugo, ihalokrunch, and snowtime pictures

--- src/Bonus/Icecream/Icecream.js
+++ src/Bonus/Icecream/Icecream.js
@@ -1,5 +1,8 @@
 import "./Icecream.scss";
 import { useHistory } from "react-router-dom";
+import fugo from "./Pictures/fugo.JPG";
+import ihalokrunch from "./Pictures/ihalokrunch.JPG";
+import snowtime from "./Pictures/snowtime.JPG";
 
 function Icecream() {
   const history = useHistory();

8bb3a820336abcfd2875f6b95f972ad81c40f6ee
Karen Kwok <kkwok98@hotmail.com>
2021-06-12 23:36:04 -0400

    utsearch description and formatted code

--- src/Projects/Project/Utsearch/Utsearch.js
+++ src/Projects/Project/Utsearch/Utsearch.js
@@ -76,6 +76,36 @@ function Utsearch() {
         <h5 className="skill-bubble">HTML</h5>
         <h5 className="skill-bubble">CSS</h5>
       </div>
+      <h4>
+        Working in a team of two, we created an online networking platform as
+        our final project for a senior year web development course at the
+        University of Toronto.
+      </h4>
+      <h4>
+        Our goal was to create a space where students could easily meet and
+        connect with other students based on their common interests. Due to the
+        ongoing pandemic, it is especially difficult for students to socialize
+        and make new friends, but that's where our app comes in.
+      </h4>
+      <h4>
+        UTSearCh is a web app that allows for real-time communication through
+        video and voice chat. Users can directly call other users, or they can
+        choose to be randomly paired with another online user to chat with.
+      </h4>
+      <h4>
+        Users can also add tags to their profiles which are descriptive keywords
+        to help other users find them. This leads to our Search feature where
+        users can search for students by their username or by tags to find
+        others with certain interests. This would easily enable students to meet
+        potential friends that share something in common with them. For example,
+        users could search for "painting" to find students that also like
+        painting.
+      </h4>
+      <h4>
+        Upon finding other students, users may choose to add them as friends, or
+        block them to prevent further interactions with them. As friends, users
+        can view each others' locations using our Maps feature.
+      </h4>
     </div>
   );
 }

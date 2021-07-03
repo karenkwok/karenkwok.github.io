e2f8f31235026e677221f20d294808dd28c3e63f
Karen Kwok <kkwok98@hotmail.com>
2021-07-02 20:18:19 -0400

    edited art page about

--- src/Bonus/Art/Art.js
+++ src/Bonus/Art/Art.js
@@ -108,10 +108,10 @@ function CenteredTabs() {
         <h4>
           As a kid, I took art lessons under various instructors in drawing,
           painting, and briefly, clay sculpting (I was really bad at
-          that). Eventually, I found a teacher who was not only an incredibly
+          this). Eventually, I found a teacher who was not only an incredibly
           gifted artist, but also a wise and compassionate mentor and friend,
           who always encouraged me to find my own style and be myself. All my
-          works below have been brought to life under his tutelage.
+          works listed here have been brought to life under his tutelage.
         </h4>
         <h4>
           After over 10 years of art lessons, I made the difficult decision to
@@ -121,7 +121,7 @@ function CenteredTabs() {
           my paints and brushes.
         </h4>
         <h4>
-          Perhaps one day, I'll pick up a brush and start painting again...
+          And here I am now.
         </h4>
       </TabPanel>
     </Paper>

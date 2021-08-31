8a258763c279efe2178cd3d060f0c14c296d9626
Karen Kwok <kkwok98@hotmail.com>
2021-08-31 00:26:04 -0400

    fixed typo

--- src/PageNotFound/PageNotFound.js
+++ src/PageNotFound/PageNotFound.js
@@ -5,7 +5,7 @@ function PageNotFound() {
     <div id="pagenotfound-section">
       <h4>Oops!</h4>
       <h4>That page does not exist.</h4>
-      <p>Click the Kk in the top left to go back to the main page.</p>
+      <p>Click the Kk at the top left to go back to the main page.</p>
       <img
         src="https://p.kindpng.com/picc/s/177-1772768_psyduck-pokemon-freetoedit-pokemon-psyduck-hd-png-download.png"
         alt="Confused Duck"

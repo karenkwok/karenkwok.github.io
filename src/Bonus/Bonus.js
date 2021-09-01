4842ad9f0dd366a73fddadbddbd6371d2a90817f
Karen Kwok <kkwok98@hotmail.com>
2021-09-01 03:19:23 -0400

    updated bonus info

--- src/Bonus/Bonus.js
+++ src/Bonus/Bonus.js
@@ -6,41 +6,48 @@ function Bonus() {
     <div id="bonus-section">
       <h2>A little bit more about me...</h2>
       <h4 className="no-margin-bottom">
-        I'm also an <span className="text-bold">amateur pianist</span> 🎹,{" "}
-        <span className="text-bold">book lover</span> 📚, and{" "}
+        I'm also an <span className="text-bold">amateur pianist</span>,{" "}
+        <span className="text-bold">book lover</span>, and{" "}
         {/* <Link to="/art"> */}
-          <span className="text-bold">retired painter</span> 🎨 {/*add purple-highlight when i finish the art page*/}
-        {/* </Link> */}
-        .
+        <span className="text-bold">retired painter</span>
+        {/*add purple-highlight when i finish the art page*/}
+        {/* </Link> */}.
       </h4>
       <h4 className="no-margin-bottom yes-margin-top">
-        I have a <span className="text-bold">pet turtle</span>. We've been together
-        for <span className="text-bold">11 years</span> now! 🐢
-      </h4>
-      <h4 className="no-margin-bottom yes-margin-top">
-        My favourite foods are <span className="text-bold">sushi</span> 🍣,{" "}
+        My favourite foods are{" "}
         <Link to="/icecream">
-          <span className="text-bold purple-highlight">ice cream</span> 🍦
+          <span className="text-bold purple-highlight">ice cream</span>
         </Link>
-        , and{" "}
+        , <span className="text-bold">sushi</span>, and{" "}
         <a
           href="https://www.popeyeschicken.ca"
           target="_blank"
           rel="noreferrer"
           id="popeyes"
         >
-          <span id="chicken" className="text-bold">
+          <span id="chicken" className="text-bold purple-highlight">
             fried chicken
-          </span>{" "}
-          🍗
-        </a>{" "}
+          </span>
+        </a>
         .
       </h4>
-      <h4 className="yes-margin-top">
-        Did I mention I'm a huuuge <span className="text-bold">Pokémon fan</span>??
-        🤪
+      <h4 className="no-margin-bottom yes-margin-top">
+        I'm currently watching{" "}
+        <a
+          href="https://www.youtube.com/watch?v=vxGP-dJd-1o"
+          target="_blank"
+          rel="noreferrer"
+          id="friends"
+        >
+          <span className="text-bold purple-highlight">Friends</span>
+        </a>{" "}
+        (and it's the funniest thing ever).
       </h4>
-      <h4 id="bonus-last-line">Thanks for stopping by! ❤️</h4>
+      <h4 className="yes-margin-top">
+        Sometimes I get a little bit crazy about{" "}
+        <span className="text-bold">Pokémon Go</span>... 🤪
+      </h4>
+      <h4 id="bonus-last-line">Thanks for stopping by! :)</h4>
     </div>
   );
 }

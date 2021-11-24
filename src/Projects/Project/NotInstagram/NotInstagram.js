fca6bb2fe8498783e77f482fe19dcd8641a36cc9
Karen Kwok <kkwok98@hotmail.com>
2021-11-24 02:24:34 -0500

    added content to notinstagram project page

--- src/Projects/Project/NotInstagram/NotInstagram.js
+++ src/Projects/Project/NotInstagram/NotInstagram.js
@@ -7,7 +7,33 @@ function NotInstagram() {
   });
 
   return (
-    <div></div>
+    <div id="notinstagram-page">
+      <div className="project-page-titles">
+        <h2 className="project-page-title">notInstagram</h2>
+      </div>
+      <p className="project-dates">Jan 2021 – Feb 2021</p>
+      <div className="skill-names">
+        <h5 className="skill-bubble">Node.js</h5>
+        <h5 className="skill-bubble">Express.js</h5>
+        <h5 className="skill-bubble">JavaScript</h5>
+        <h5 className="skill-bubble">HTML</h5>
+        <h5 className="skill-bubble">CSS</h5>
+      </div>
+      <h4>
+        As a project for a web development course, I created a web gallery (
+        <span className="text-italics">not</span> like{" "}
+        <a
+          href="https://www.instagram.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="purple-highlight"
+        >
+          Instagram
+        </a>
+        ;) for users to post and comment on images.
+      </h4>
+      <h4>After the course, I deployed it for fun. 🤪</h4>
+    </div>
   );
 }
 

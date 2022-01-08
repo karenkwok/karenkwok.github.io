5eecf35611eb5ce92a119d2deb9b10b1a4ab7c0a
Karen Kwok <kkwok98@hotmail.com>
2022-01-08 00:54:35 -0500

    removed bold text

--- src/Bonus/Bonus.js
+++ src/Bonus/Bonus.js
@@ -6,34 +6,29 @@ function Bonus() {
     <div id="bonus-section">
       <h2>A little bit more about me...</h2>
       <h4 className="no-margin-bottom">
-        I'm also an <span className="text-bold">amateur pianist</span>,{" "}
-        <span className="text-bold">book lover</span>, and{" "}
+        I'm also an amateur pianist, book lover, and{" "}
         {/* <Link to="/art"> */}
-        <span className="text-bold">retired painter</span>
+        retired painter
         {/*add purple-highlight when i finish the art page*/}
         {/* </Link> */}.
       </h4>
       <h4 className="no-margin-bottom yes-margin-top">
         My favourite foods are{" "}
         <Link to="/icecream">
-          <span className="text-bold purple-highlight">ice cream</span>
+          <span className="purple-highlight">ice cream</span>
         </Link>
-        , <span className="text-bold">sushi</span>, and{" "}
+        , sushi, and{" "}
         <a
           href="https://www.popeyeschicken.ca"
           target="_blank"
           rel="noreferrer"
           id="popeyes"
         >
-          <span id="chicken" className="text-bold">
-            fried chicken
-          </span>
+          <span id="chicken">fried chicken</span>
         </a>
         .
       </h4>
-      <h4 className="yes-margin-top">
-        Sometimes I play <span className="text-bold">Pokémon GO</span>... 🤪
-      </h4>
+      <h4 className="yes-margin-top">Sometimes I play Pokémon GO... 🤪</h4>
       <h4 id="bonus-last-line">Thanks for stopping by! :)</h4>
     </div>
   );

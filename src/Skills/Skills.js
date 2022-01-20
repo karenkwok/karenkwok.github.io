ec97399ab1e5720382fba7769afbe16ee775318a
Karen Kwok <kkwok98@hotmail.com>
2022-01-20 00:17:59 -0500

    rearranged skills

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -12,11 +12,10 @@ function Skills() {
             <h5 className="skill-bubble">Redux</h5>
             <h5 className="skill-bubble">Node.js</h5>
             <h5 className="skill-bubble">Express.js</h5>
-            <h5 className="skill-bubble">Django</h5>
             <h5 className="skill-bubble">PostgreSQL</h5>
             <h5 className="skill-bubble">MongoDB</h5>
-            <h5 className="skill-bubble">JavaScript</h5>
             <h5 className="skill-bubble">GraphQL</h5>
+            <h5 className="skill-bubble">Django</h5>
             <h5 className="skill-bubble">SCSS</h5>
             <h5 className="skill-bubble">HTML</h5>
             <h5 className="skill-bubble">CSS</h5>
@@ -26,6 +25,7 @@ function Skills() {
           <h3>Languages</h3>
           <div className="skill-names">
             <h5 className="skill-bubble">Python</h5>
+            <h5 className="skill-bubble">JavaScript</h5>
             <h5 className="skill-bubble">Java</h5>
             <h5 className="skill-bubble">C#</h5>
             <h5 className="skill-bubble">C</h5>
@@ -35,9 +35,9 @@ function Skills() {
         <div id="skill-technologies">
           <h3>Technologies</h3>
           <div className="skill-names">
+            <h5 className="skill-bubble">Git</h5>
             <h5 className="skill-bubble">Heroku</h5>
             <h5 className="skill-bubble">Unity</h5>
-            <h5 className="skill-bubble">Git</h5>
           </div>
         </div>
       </div>

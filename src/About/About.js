b9e1dfe6e73259402ae7d3f10f790bc48af0ad35
Karen Kwok <kkwok98@hotmail.com>
2022-03-10 22:50:37 -0500

    updated about section

--- src/About/About.js
+++ src/About/About.js
@@ -5,10 +5,20 @@ function About() {
     <div id="about-section">
       <h4>Hey, I'm Karen! 👋</h4>
       <h4 id="about-second-line">
-        I'm a full stack developer and recent grad from the University of
-        Toronto.
+        I'm a software developer and recent grad from the University of Toronto.
       </h4>
-      <h4 id="about-third-line">Currently, I'm open to opportunities!</h4>
+      <h4 id="about-third-line">
+        Currently @{" "}
+        <a
+          href="https://milanosoftware.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="purple-highlight"
+        >
+          Milano Software
+        </a>
+        .
+      </h4>
     </div>
   );
 }

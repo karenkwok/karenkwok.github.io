4160c03c844ad3bc6792105661c6d3fa34595705
Karen Kwok <kkwok98@hotmail.com>
2023-05-09 23:13:14 -0400

    added particles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,99 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import Particles from 'react-particles';
+import { loadFull } from 'tsparticles';
 
 import Title from './components/Title';
 import './App.scss';
 
 const App = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
   return (
     <div id='App'>
-      <Title />
+      <Particles
+        id='tsparticles'
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: '#00020a',
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push',
+              },
+              onHover: {
+                enable: false,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 10,
+              },
+              repulse: {
+                distance: 20,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#fcf5e7',
+            },
+            collisions: {
+              enable: false,
+            },
+            move: {
+              directions: 'none',
+              enable: true,
+              outModes: {
+                default: 'bounce',
+              },
+              random: false,
+              speed: 0.2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: { min: 0.1, max: 0.5 },
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: { min: 0.5, max: 4 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+
+      <div style={{ zIndex: 999 }}>
+        <Title />
+      </div>
     </div>
   );
 };

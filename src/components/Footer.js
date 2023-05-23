b1af4859fd776043f10268f409e82c57a4d72794
Karen Kwok <kkwok98@hotmail.com>
2023-05-23 19:32:56 -0400

    added tooltip to email and styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedinIn, faGithubAlt } from '@fortawesome/free-brands-svg-icons';
+import { styled } from '@mui/material/styles';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import Zoom from '@mui/material/Zoom';
 
 import './Footer.scss';
 
+const CustomTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} arrow classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.arrow}`]: {
+    color: '#ffffff1f',
+  },
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: '#ffffff1f',
+    color: '#fcf5e7',
+    padding: theme.spacing(2),
+    borderRadius: '10px',
+    fontSize: 20,
+    fontWeight: 'lighter',
+  },
+}));
+
 const Footer = () => {
+  const email = 'karenkwok013@gmail.com';
+  const [tooltipText, setTooltipText] = useState('Click to copy');
+
+  const updateTooltipText = () => {
+    navigator.clipboard.writeText(email);
+    setTooltipText('Copied!');
+  };
+
+  const resetTooltipText = () => {
+    setTooltipText('Click to copy');
+  };
+
   return (
     <footer>
       <div id='footer-links'>
@@ -27,10 +58,20 @@ const Footer = () => {
           </a>
         </div>
       </div>
-      <div
+      <CustomTooltip
+        title={tooltipText}
+        placement='top'
+        TransitionComponent={Zoom}
+        arrow
       >
-        karenkwok013@gmail.com
-      </div>
+        <div
+          id='footer-email'
+          onClick={updateTooltipText}
+          onMouseOut={resetTooltipText}
+        >
+          karenkwok013@gmail.com
+        </div>
+      </CustomTooltip>
     </footer>
   );
 };

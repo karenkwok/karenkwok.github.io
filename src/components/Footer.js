28c3e880421911e25b89895ba11d8b31efa566f7
Karen Kwok <kkwok98@hotmail.com>
2023-05-23 17:15:22 -0400

    added social media buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 
 import './Footer.scss';
 
 const Footer = () => {
   return (
     <footer>
-      <div>karenkwok013@gmail.com</div>
+      <div id='footer-links'>
+        <div id='footer-links-linkedin'>
+          <a
+            href='https://www.linkedin.com/in/karen-kwok/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
+        </div>
+        <div id='footer-links-github'>
+          <a
+            href='https://github.com/karenkwok'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FontAwesomeIcon icon={faGithubAlt} />
+          </a>
+        </div>
+      </div>
+      <div
+      >
+        karenkwok013@gmail.com
+      </div>
     </footer>
   );
 };

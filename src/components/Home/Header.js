a30dcb6d8b8101390ecb0ceeb3330922e17be197
Karen Kwok <kkwok98@hotmail.com>
2023-06-08 15:54:31 -0400

    linked mainpage to icecream page

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import './Header.scss';
 
 const Header = () => {
+  const location = useLocation();
+  const destination = location.pathname === '/' ? '/icecream' : '/';
+
   return (
     <header>
-      <div id='logo'>Kk</div>
+      <div id='logo'>
+        <Link to={destination}>Kk</Link>
+      </div>
     </header>
   );
 };

1f5563d7fab6eefe7e3c31b02bb348b12c08b80a
Karen Kwok <kkwok98@hotmail.com>
2023-06-15 00:20:14 -0400

    added bumble sections content

--- src/components/Bumble/Bumble.js
+++ src/components/Bumble/Bumble.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import BumbleSection from '../UI/BumbleSection';
+import BumbleItemContainer from '../UI/BumbleItemContainer';
+import BumbleItem from '../UI/BumbleItem';
 import './Bumble.scss';
 
 const Bumble = () => {
@@ -21,6 +23,87 @@ const Bumble = () => {
           I deleted my Bumble account so I thought it'd be fun to make this. 🤪
         </p>
         <p>⚡️ 💫 ✨</p>
+      </BumbleSection>
+      <BumbleSection>
+        <h3>My basics</h3>
+        <BumbleItemContainer>
+          <BumbleItem>💻&nbsp;&nbsp;Software Developer</BumbleItem>
+          <BumbleItem>🎓&nbsp;&nbsp;University of Toronto</BumbleItem>
+          <BumbleItem>🎹&nbsp;&nbsp;Amateur Pianist</BumbleItem>
+          <BumbleItem>🐢&nbsp;&nbsp;Turtle Mom</BumbleItem>
+        </BumbleItemContainer>
+      </BumbleSection>
+      <BumbleSection>
+        <h3>Binge-watch recommendations...</h3>
+        <BumbleItemContainer>
+          <BumbleItem>🪄&nbsp;&nbsp;Harry Potter</BumbleItem>
+          <BumbleItem>🌱&nbsp;&nbsp;Studio Ghibli</BumbleItem>
+          <BumbleItem>☕️&nbsp;&nbsp;Friends</BumbleItem>
+        </BumbleItemContainer>
+      </BumbleSection>
+      <BumbleSection>
+        <h3>My Hogwarts House is...</h3>
+        <BumbleItemContainer>
+          <BumbleItem>🐍&nbsp;&nbsp;Slytherin</BumbleItem>
+        </BumbleItemContainer>
+      </BumbleSection>
+      <BumbleSection>
+        <h3>My top 5 fav anime...</h3>
+        <BumbleItemContainer>
+          <BumbleItem>🖤&nbsp;&nbsp;Bleach</BumbleItem>
+          <BumbleItem>❤️&nbsp;&nbsp;InuYasha</BumbleItem>
+          <BumbleItem>🧡&nbsp;&nbsp;Naruto</BumbleItem>
+          <BumbleItem>💙&nbsp;&nbsp;Samurai Champloo</BumbleItem>
+          <BumbleItem>💚&nbsp;&nbsp;Chainsaw Man</BumbleItem>
+        </BumbleItemContainer>
+      </BumbleSection>
+      <BumbleSection>
+        <h3>My Pokémon team...</h3>
+        <BumbleItemContainer>
+          <BumbleItem>🔵&nbsp;&nbsp;Squirtle</BumbleItem>
+          <BumbleItem>⚫️&nbsp;&nbsp;Houndoom</BumbleItem>
+          <BumbleItem>🟡&nbsp;&nbsp;Flaaffy</BumbleItem>
+          <BumbleItem>⚪️&nbsp;&nbsp;Teddiursa</BumbleItem>
+          <BumbleItem>🔴&nbsp;&nbsp;Arcanine</BumbleItem>
+          <BumbleItem>🟤&nbsp;&nbsp;Flygon</BumbleItem>
+        </BumbleItemContainer>
+      </BumbleSection>
+      <BumbleSection>
+        <h3>Food I could eat everyday...</h3>
+        <BumbleItemContainer>
+          <BumbleItem>🍣&nbsp;&nbsp;Sushi</BumbleItem>
+          <BumbleItem>🍨&nbsp;&nbsp;Ice Cream</BumbleItem>
+          <BumbleItem>🍳&nbsp;&nbsp;Brunch</BumbleItem>
+          <BumbleItem>🍔&nbsp;&nbsp;Shake Shack</BumbleItem>
+          <BumbleItem>🍟&nbsp;&nbsp;Yeet Hay (熱氣) food</BumbleItem>
+          <BumbleItem>🧀&nbsp;&nbsp;anything cheesy</BumbleItem>
+        </BumbleItemContainer>
+      </BumbleSection>
+      <BumbleSection>
+        <h3>Best ice cream flavours...</h3>
+        <BumbleItemContainer>
+          <BumbleItem>🍪&nbsp;&nbsp;Cookies 'n Cream / Oreo</BumbleItem>
+          <BumbleItem>🍵&nbsp;&nbsp;Matcha</BumbleItem>
+          <BumbleItem>🍫&nbsp;&nbsp;White Chocolate</BumbleItem>
+          <BumbleItem>🫐&nbsp;&nbsp;Blueberry</BumbleItem>
+          <BumbleItem>🥜&nbsp;&nbsp;Pistachio</BumbleItem>
+        </BumbleItemContainer>
+      </BumbleSection>
+      <BumbleSection>
+        <h3>Cool place I've been...</h3>
+        <BumbleItemContainer>
+          <BumbleItem>🇺🇸&nbsp;&nbsp;Los Angeles, CA</BumbleItem>
+        </BumbleItemContainer>
+      </BumbleSection>
+      <BumbleSection>
+        <h3>Weird flex but ok...</h3>
+        <BumbleItemContainer>
+          <BumbleItem>😎&nbsp;&nbsp;I have a lot of Pokémon cards</BumbleItem>
+          <BumbleItem>✌️&nbsp;&nbsp;Level 43 on Pokémon GO</BumbleItem>
+          <BumbleItem>
+            🏆&nbsp;&nbsp;Can list the first 51 Pokémon in order
+          </BumbleItem>
+        </BumbleItemContainer>
       </BumbleSection>
     </div>
   );
